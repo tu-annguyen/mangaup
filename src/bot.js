b179772108a67157dd6076aff7ab5514c6384f31
Tu-An Nguyen <t.nhunhu.ng@gmail.com>
2020-11-12 23:46:55 -0800

    Implemented a dictionary for manga titles and URLs

--- src/bot.js
+++ src/bot.js
@@ -1,16 +1,12 @@
 require('dotenv').config();
 
 const { Client } = require('discord.js');
-const client = new Client({
-  partials: ['MESSAGE', 'REACTION']
-});
+const client = new Client();
 
-const URL = 'https://www.mangaupdates.com/rss.php';
 const fetch = require('node-fetch');
 const JSDOM = require('jsdom');
-const DOMParser = new JSDOM.JSDOM('').window.DOMParser;
-const parser = new DOMParser;
-const kaguya = 'https://www.mangaupdates.com/series.html?id=121872';
+
+const mangaDict = {};
 
 const PREFIX = "$";
 
@@ -26,24 +22,47 @@ client.on('message', (message) => {
       .substring(PREFIX.length)
       .split(/\s+/);
 
-    if (CMD_NAME === 'check-kaguya') {
-      if (args.length === 0) {
-        fetch(URL)
+    if (CMD_NAME === 'add') {
+      if (args.length === 2) {
+        // Edit regex to limit only to mangaupdates.com/series.html?id=
+        if (/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/.test(args[1])) {
+          if (!(args[0] in mangaDict)) {
+            mangaDict[args[0]] = args[1];
+            message.reply(`Added \`${args[0]}\` to manga dictionary.`);
+          } else message.reply(`\`${args[0]}\` already exists in the manga dictionary.`);
+        } else message.channel.send('Please enter a valid URL. The command format should be: `$add <manga-name> https://www.mangaupdates.com/series.html?id=<mangaupdates-ID>`');
+      } else message.channel.send('Invalid command. The command format should be: `$add <manga-name> https://www.mangaupdates.com/series.html?id=<mangaupdates-ID>`');
+    }
+    
+    if (CMD_NAME === 'check') {
+      if (args.length === 1) {
+        mangaName = args[0]
+        const mangaUpdatesRSS = 'https://www.mangaupdates.com/rss.php';
+        const DOMParser = new JSDOM.JSDOM('').window.DOMParser;
+        const parser = new DOMParser;
+        fetch(mangaUpdatesRSS)
         .then(response => response.text())
         .then(str => parser.parseFromString(str, "text/xml"))
         .then(data => {
           const links = data.getElementsByTagName('link');
           var updated = 0;
           for(i = 1; i < links.length; i++) {
-            if(links[i].textContent === kaguya) {
-              updated = 1;
+            if(args[0] in mangaDict) {
+              if(links[i].textContent === mangaDict[args[0]]) {
+                updated = 1;
+                message.reply(`A new chapter of \`${args[0]}\` has been released.`);
+                break;
+              }
+            } else {
+              updated = -1;
+              message.reply(`\`${args[0]}\` not found in manga dictionary.`);
+              break;
             }
           }
-          if(updated) message.channel.send('A new chapter of Kaguya has been released.');
-          else message.channel.send('No new chapter of Kaguya has been found.');
+          if(!updated) message.reply(`No new chapter of \`${args[0]}\` has been released.`);
         });
       } else {
-        message.channel.send('Invalid command. Please remove arguments.')
+        message.channel.send('Invalid command. The command format should be: `$check <manga-name>`')
       }
     }
   }
